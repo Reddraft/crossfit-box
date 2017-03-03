74bce0307a7cfdc9beb2c62398a8ad38dac193a9
Ricardo <rick@reddraft.com>
2017-03-03 09:48:58 -0600

    Add click instead of hover on mobile dvices

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+  $('a').on('click touchend', function(e) {
+    var el = $(this);
+    var link = el.attr('href');
+    window.location = link;
+  });
+
 
   /***********************
   *** STICKY NAVIGATION **
