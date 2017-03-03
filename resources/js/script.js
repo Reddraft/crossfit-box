0abde577efee70e63c2388ea12acef5598c8fac5
Ricardo <rick@reddraft.com>
2017-03-03 11:04:56 -0600

    Add new script to make hover work in mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 
-  // $('a').on('click touchend', function(e) {
-  //   var el = $(this);
-  //   var link = el.attr('href');
-  //   window.location = link;
-  // });
-
+  $('.hover').bind('touchstart touchend', function(e) {
+      e.preventDefault();
+      $(this).toggleClass('hover_effect');
+  });
 
   /***********************
   *** STICKY NAVIGATION **
