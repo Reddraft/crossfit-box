6b4c1d70f0347fbd8e6208d4e7ba8a74e875c70a
Ricardo <rick@reddraft.com>
2017-03-04 17:16:05 -0600

    Add and style google maps

--- resources/js/script.js
+++ resources/js/script.js
@@ -200,8 +200,6 @@ $(document).ready(function() {
 
   });
 
-
-
   /*********************************************************
    click instead of hover on program images on mobile device
   *********************************************************/
@@ -209,6 +207,254 @@ $(document).ready(function() {
   [].slice.call( document.querySelectorAll('figure.program-photo figcaption a') ).forEach( function(el) {
     el.onclick = function() { return false; }
   } );
+
+  /******************************
+  *** GMAPS **
+  *******************************/
+
+  var map = new GMaps({
+    div: '.map',
+    lat: 32.873229,
+    lng: -96.935465,
+    zoom: 16,
+    draggable: false,
+    scrollwheel: false,
+    resize: function() {
+      this.setCenter({lat: 32.873229, lng: -96.935465 });
+    },
+
+    styles: [
+      {
+          "featureType": "administrative",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "color": "#444444"
+              }
+          ]
+      },
+      {
+          "featureType": "administrative.locality",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#f2f2f2"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              },
+              {
+                  "saturation": "-65"
+              },
+              {
+                  "lightness": "45"
+              },
+              {
+                  "gamma": "1.78"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 45
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "saturation": "-33"
+              },
+              {
+                  "lightness": "22"
+              },
+              {
+                  "gamma": "2.08"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.station.airport",
+          "elementType": "geometry",
+          "stylers": [
+              {
+                  "gamma": "2.08"
+              },
+              {
+                  "hue": "#ffa200"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.station.airport",
+          "elementType": "labels",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.station.rail",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit.station.rail",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              },
+              {
+                  "saturation": "-55"
+              },
+              {
+                  "lightness": "-2"
+              },
+              {
+                  "gamma": "1.88"
+              },
+              {
+                  "hue": "#ffab00"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#bbd9e5"
+              },
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      }
+    ]
+  });
+
+  map.addMarker({
+    lat: 32.873229,
+    lng: -96.935465,
+    title: 'crossfitbox',
+    click: function(e) {
+      window.open('https://goo.gl/maps/rfWEk1G4yG62', '_blank');
+    },
+    icon: 'resources/img/marker.png',
+    animation: google.maps.Animation.BOUNCE
+  });
+
+  var resizingTimeOut;
+  $(window).resize(function() {
+    clearTimeout(resizingTimeOut);
+    resizingTimeOut = setTimeout(function() {
+      map.refresh();
+    }, 200);
+  });
 
 
 });
